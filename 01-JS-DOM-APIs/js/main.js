f9d8397486c286d85942cc81f446da86ab716262
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-10-19 22:38:34 -0300

    update exersise 1

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -1,9 +1,11 @@
-function fadeIn(e) {
-    var x = document.getElementById(e);
-    x.style.visibility = "visible";
+function fadeIn(id) {
+    var x = document.getElementById(id);
+    x.classList.add('on-load');
 }
 
-//window.addEventListener("load", fadeIn("hidden-element"));
+//window.addEventListener("load", fadeIn("hidden-element")); // Why it don't works?
+
 window.addEventListener("load", function(){
     fadeIn("hidden-element");
 });
+
