211971408aaf7865b473831e0f305739c80779c0
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-10-27 17:08:59 -0300

    Exersise 1 - Creating classes

---
+++ 02-OOP-inheritance/main.js
@@ -0,0 +1,60 @@
+
+class Movie {
+    constructor(title, year, duration) {
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+    }
+
+    play() {
+        console.log("playing");
+    }
+
+    pause() {
+        console.log("paused");
+    }
+
+    resume() {
+        console.log("resumed");
+    }
+}
+
+class Actor {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class EventEmitter {
+    constructor(){
+        this.events = {};
+    }
+
+    on(eventName, callback){
+
+        if(!this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+        this.events[eventName].push(callback);
+    }
+
+    emit(eventName){
+        const event = this.events[eventName];
+        if(event){
+            event.forEach(callback => {
+                callback.call(this);
+            });
+        }
+    }
+
+    off(eventName, callback){
+        if(this.events[eventName]){
+            this.events[eventName] = this.events[eventName].filter(eventCallback => callback !== eventCallback);
+        }
+    }
+}
+
+const mov1 = new Movie("Terminator", 1984, 108);
+const mov2 = new Movie("Titanic", 1997, 195);
+
