6d2584375761d7b24554e179a9b15556b34fdf2e
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-11-07 20:41:21 -0300

    Topic 1.4 - Text Components withnesting

--- 06-React-Native/topic-1/App.js
+++ 06-React-Native/topic-1/App.js
@@ -12,6 +12,17 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <ScrollView style={styles.mainScroll}>
           <BigBox color="blue"/>
+          <Text style={styles.baseText}>
+            <Text style={styles.titleText}>
+              My app is awesome! ... or not.
+            </Text>
+            <Text>
+              To be or not to be...
+            </Text>
+            <Text style={styles.anotherText}>
+              Bold text rocks!
+            </Text>
+          </Text>
           <LittleBox color="green"/>
           <MediumBox color="lightblue"/>
           <BigBox color="red"/>
@@ -36,5 +47,20 @@ const styles = StyleSheet.create({
     height: '50%',
     backgroundColor: 'orange',
 
+  },
+
+  baseText: {
+    fontFamily: 'monospace',
+    fontSize: 20
+  },
+
+  titleText: {
+    fontSize: 60,
+    color: 'red'
+  },
+  anotherText: {
+    fontSize: 40,
+    color: 'blue',
+    fontWeight: 'bold'
   }
 });
