0bcc31678318a4e99b1e4df160f1670791eddf7b
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-11-09 14:29:11 -0300

    Topic 2.4 - Box's heigth

--- 06-React-Native/topic-2/styles/mystyle.js
+++ 06-React-Native/topic-2/styles/mystyle.js
@@ -12,27 +12,27 @@ const styles = StyleSheet.create({
     box1: {
       margin: '5%',
       width: '30%',
-      height: '15%',
+      height: '50%',
       backgroundColor: '#F12B2B'
     },
 
     box2: {
       margin: '5%',
       width: '30%',
-      height: '15%',
-      backgroundColor: '#8100E1'
+      height: '30%',
+      backgroundColor: '#8100E1',
+
     },
 
     box3: {
       margin: '5%',
       width: '30%',
-      height: '15%',
+      height: '20%',
       backgroundColor: '#FFFFFF'
     },
 
     boxesRadius: {
       borderRadius: 1,
-      marginTop: '10%'
     }
 
 });
