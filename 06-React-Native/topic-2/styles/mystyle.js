d6658cdb1841bb97f1f4f377813d745d7d9373f0
Carrizo Santiago <santiicaarrizo@gmail.com>
2018-11-09 15:07:41 -0300

    Topic 2.5 and 2.6 - Layout and Platform select

--- 06-React-Native/topic-2/styles/mystyle.js
+++ 06-React-Native/topic-2/styles/mystyle.js
@@ -1,38 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#1597E8',
-      alignItems: 'flex-start',
       justifyContent: 'space-around',
-      flexDirection: 'column'  // FLEX DIRECTION ROW FOR FIRST LAYOUT (EXERSISE 3)
+      flexDirection: 'column',  // FLEX DIRECTION ROW FOR FIRST LAYOUT (EXERSISE 3)
+      ...Platform.select({
+        ios: {
+          backgroundColor: 'grey',
+        },
+        android: {
+          backgroundColor: 'green',
+        },
+      }),
     },
 
     box1: {
-      margin: '5%',
-      width: '30%',
-      height: '50%',
+      width: '25%',
+      height: '15%',
       backgroundColor: '#F12B2B'
     },
 
     box2: {
-      margin: '5%',
-      width: '30%',
-      height: '30%',
-      backgroundColor: '#8100E1',
-
+      width: '25%',
+      height: '15%',
+      backgroundColor: '#FFFFFF',
+      alignSelf: 'center',
     },
 
     box3: {
-      margin: '5%',
-      width: '30%',
-      height: '20%',
-      backgroundColor: '#FFFFFF'
+      width: '25%',
+      height: '15%',
+      backgroundColor: '#8100E1',
+      alignSelf: 'flex-end'
     },
 
     boxesRadius: {
       borderRadius: 1,
+      margin: '5%'
     }
 
 });
